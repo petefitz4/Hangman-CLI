8550adea521943d2e9c7431e61b69044750f521d
petefitz4 <petefitz4@gmail.com>
2017-11-26 17:29:37 -0600

    added constructor files

---
+++ letter.js
@@ -0,0 +1,5 @@
+
+
+//Used for each letter in the current word. 
+//Each letter object should either display an underlying character, or a blank placeholder (such as an underscore), depending on whether or not the user has guessed the letter. 
+//This should contain letter specific logic and data
---
+++ word.js
@@ -0,0 +1,4 @@
+
+
+//create an object representing the current word the user is attempting to guess. 
+//This should contain word specific logic and data
